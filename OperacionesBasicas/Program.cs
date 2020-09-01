8d3f66ec58bcd49de8dd8b1870fbaacf6c0eb9c5
Cristhian De Castro <cristhiancastro01@gmail.com>
2020-08-31 20:02:27 -0400

    Agregar archivos de proyecto.

---
+++ OperacionesBasicas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperacionesBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1 = 50;
+            double numero2 = 75;
+
+            Console.WriteLine("Digite el primer numero");
+
+            Console.WriteLine("Digite el segundo numero");
+
+
+            Console.WriteLine("Su suma es: {0}", numero1 + numero2);
+            Console.WriteLine("Su resta es: {0}",numero1 - numero2);
+            Console.WriteLine("Su multiplicacion es: {0}", numero1 * numero2);
+            Console.WriteLine("Su Division es: {0}", numero1 / numero2);
+            Console.WriteLine("Su modulo es: {0}", numero1 % numero2);
+
+        }
+    }
+}
